f6a1f7a7882118923329b20ddc9932bd96ad4011
jdgreen9 <80784979+jdgreen9@users.noreply.github.com>
2021-04-18 21:04:16 -0500

    Add files via upload
    cleaned up the form, adding some fields

--- FoodBank/Form1.cs
+++ FoodBank/Form1.cs
@@ -41,5 +41,30 @@ namespace FoodBank
         {
 
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
